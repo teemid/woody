797b8d80952adc7dd2b2ed8dea8394988fdf1f61
Emil Grunt <emil.grunt@lumenia.no>
2016-10-06 22:20:27 +0200

    Adds cmake and changes the build script to invoke cmake and msbuild

--- build.bat
+++ build.bat
@@ -2,34 +2,9 @@
 
 IF NOT EXIST build MKDIR build
 
-if "%1" == "msvc" GOTO msvc
-if "%1" == "clang" GOTO clang
+PUSHD build
 
-:msvc
-    SET COMPILER_FLAGS=/nologo /Od /W4 /wd4127 /wd4996 /Zi /Fobuild\ /Fdbuild\ /c /I "include"
+cmake ..
+msbuild woody.vcxproj
 
-    FOR /r %%f IN ("src\*.c") DO cl %COMPILER_FLAGS% "%%f" %MACROS%
-
-    SET LINKER_FLAGS=/NOLOGO /DEBUG
-
-    PUSHD build
-
-    link %LINKER_FLAGS% /OUT:woody.exe *.obj
-
-    POPD
-
-    GOTO end
-
-:clang
-    REM -Werror - turn warnings into errors
-    REM -g - generate complete debugging information
-    REM -pedantic-errors - request error if a feature from a later standard is used in an earlier mode
-
-    SET COMPILER_FLAGS=-Werror -g -pedantic-errors
-
-    FOR /r %%f IN ("src/*.c") DO clang %COMPILER_FLAGS% "%%f" %MACROS%
-
-    GOTO end
-
-:end
-    echo "Finished!"
+POPD
