64cdfe6e8a7b6c712ca1f33178ba06edc5be556d
Emil Grunt <emilgrunt@gmail.com>
2016-10-21 02:33:23 +0200

    Adds zero function to memory.h

--- include/woody_memory.h
+++ include/woody_memory.h
@@ -7,6 +7,7 @@
 #define ZeroAllocate(element_size, element_count) calloc(element_size, element_count)
 #define Reallocate(old_memory, bytes) realloc(old_memory, bytes)
 #define Deallocate(pointer) free(pointer)
+#define Zero(memory, bytes) memset((void *)memory, 0, bytes)
 
 #define Buffer(type, elements) (type *)malloc(sizeof(type) * elements)
 #define ResizeBuffer(type, old_buffer, new_size) (type *)realloc(old_buffer, sizeof(type) * new_size)
